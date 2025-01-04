60614f6711916b9a2aa6fffc32c2c8bcb475f4bf
Ryan Laleh <33795357RyN21@users.noreply.github.com>
2025-01-04 10:57:50 -0700

    Add game spec and tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Game do
+  let(:game) { Game.new }
+
+  describe ".new" do
+    it "has a game window" do
+      expect(game.game_window).to be_an_instance_of GameWindow
+    end
+    it "has a input_manager" do
+      expect(game.input_manager).to eq InputManager
+    end
+  end
+  describe ".start" do
+    it "shows the game window" do
+      expect_any_instance_of(GameWindow).to receive(:show)
+      game.start
+    end
+  end
+end
