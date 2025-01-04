7d9bdfecc3511715e50b8786996f9fb9dbf16a2c
Ryan Laleh <33795357RyN21@users.noreply.github.com>
2025-01-03 23:17:31 -0700

    rspec updated proper way

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,21 @@
-class Game
-  def name
-    'Game'
+require "gosu"
+
+WIDTH  = 800
+HEIGHT = 600
+TITLE  = "Game"
+
+class Game < Gosu::Window
+  def initialize
+    super WIDTH, HEIGHT
+    self.caption = TITLE
   end
+
+  def update
+  end
+
+  def draw
+  end
+
 end
+
+# Game.new.show
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require 'spec_helper.rb'
 
 describe "Game" do
   describe ".name" do
